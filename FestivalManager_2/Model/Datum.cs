1ae5876b1a2c0beed12cec849fad2da1ad6ec972
Jeroen <valcke_jeroen@hotmail.com>
2013-12-15 16:22:43 +0100

    GenresUI + MainWindowUI

--- FestivalManager_2/Model/Datum.cs
+++ FestivalManager_2/Model/Datum.cs
@@ -12,8 +12,31 @@ namespace FestivalManager_2.Model
         public DateTime Date { get; set; }
 
         public override string ToString()
-        {          
-            return this.Date.ToString();
+        {
+            return ConvertDagToNederlands(this.Date.DayOfWeek.ToString()) + " (" + this.Date.Day + "/" + this.Date.Month + "/" + this.Date.Year + ")";
+        }
+
+        private string ConvertDagToNederlands(string s)
+        {
+            switch(s)
+            {
+                case "Monday":
+                    return "Maandag";
+                case "Tuesday":
+                    return "Dinsdag";
+                case "Wednesday":
+                    return "Woensdag";
+                case "Thursday":
+                    return "Donderdag";
+                case "Friday":
+                    return "Vrijdag";
+                case "Saturday":
+                    return "Zaterdag";
+                case "Sunday":
+                    return "Zondag";
+                default:
+                    return null;
+            }
         }
     }
 }
