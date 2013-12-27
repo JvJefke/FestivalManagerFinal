d554ac0d7816a545030f25a9b8e83a7de5937f5b
Jeroen <valcke_jeroen@hotmail.com>
2013-12-27 15:56:49 +0100

    OptredenToDeleteVisibleConverter created and implemented

---
+++ FestivalManager_2/View/Converters/OptredentToDeleteVisibleConverter.cs
@@ -0,0 +1,28 @@
+﻿using FestivalManager_2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FestivalManager_2.View.Converters
+{
+    class OptredentToDeleteVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Optreden o = (Optreden)value;
+            if (o == null)
+                return Visibility.Collapsed;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
